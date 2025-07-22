5d3a23ea966df61cbff554c5f12e3e961c470b1a
Чарненко Дмитрий Павлович <charnenka@sc.softclub.by>
2025-07-22 18:19:30 +0300

    set up security config and login, registration pages

---
+++ booking/src/main/java/com/example/library/entity/AppUserEntity.java
@@ -0,0 +1,34 @@
+package com.example.library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class AppUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, name = "Username")
+    private String username;
+    @Column(nullable = false, name = "Password")
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roles;
+}
